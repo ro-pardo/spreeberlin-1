0ab1166f23d5571d870a663d4de1a748264d8563
JuliBorn <juliusbornmuc@gmail.com>
2022-05-13 23:06:25 +0200

    asdas

--- pages/api/projekte/index.js
+++ pages/api/projekte/index.js
@@ -24,19 +24,41 @@ if (MODE == 'production') {
 export default async function handler(req, res) {
     //console.log("mariadb api route");
     //console.log("ENV", process.env.NODE_ENV);
-    const conn = await mariadb.createConnection({
-        host: connData.host,
-        user: connData.user,
-        password: connData.password,
-        database: connData.database,
-    });
     // const conn = await mariadb.createConnection({
-    //     host: 'localhost',
-    //     user: 'user',
-    //     password: '7h!1rwQ40',
-    //     database: 'spreewater',
+    //     host: connData.host,
+    //     user: connData.user,
+    //     password: connData.password,
+    //     database: connData.database,
     // });
-    const result = await conn.query('select * from marker_projekte');
+    // // const conn = await mariadb.createConnection({
+    // //     host: 'localhost',
+    // //     user: 'user',
+    // //     password: '7h!1rwQ40',
+    // //     database: 'spreewater',
+    // // });
+    // const result = await conn.query('select * from marker_projekte');
 
-    return res.status(200).json(result);
+    // return res.status(200).json(result);
+
+
+    console.log(process.env.DATABASE_URL);
+
+    const client = new Client({
+        user: 'rtknowvkwcapnb',
+        host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
+        database: 'd3o15bh1avgeaj',
+        password:
+            '0768373982233b574dca3037ac28178802ddedbd2ddcdab0f6397ac02f89384a',
+        port: 5432,
+    });
+
+    await client.connect();
+
+    const result = await client.query('SELECT * from marker_projekte');
+
+    //console.log("postgres result", result.fields); // Hello world!
+
+    await client.end();
+
+    return res.status(200).json(result.rows);
 }
