8db216fb87c61c2726b9a63920da80e98e048ab8
JuliBorn <juliusbornmuc@gmail.com>
2022-05-14 14:13:53 +0200

    prisma connection limit stuff

--- pages/api/geschichte/index.js
+++ pages/api/geschichte/index.js
@@ -3,11 +3,9 @@ const mariadb = require('mariadb');
 
 import config from '../../../config';
 
-const { Client } = require('pg');
+import prisma from '../../../lib/prisma.tsx';
 
 const MODE = config.mode;
-
-import { PrismaClient } from '@prisma/client';
 
 let connData = {};
 
@@ -27,14 +25,14 @@ if (MODE == 'production') {
     };
 
 export default async function handler(req, res) {
+
     console.log(process.env.DATABASE_URL);
 
-    const prisma = new PrismaClient();
     const marker_geschichte = await prisma.marker_geschichte.findMany();
+
     //const feed = await prisma.post.findMany();
 
     console.log('prisma feed', marker_geschichte);
-
 
     return res.status(200).json(marker_geschichte);
 }
