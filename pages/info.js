09d5209a52b7bad523afb79c0732ad6bb5f3e7d1
JuliBorn <juliusbornmuc@gmail.com>
2022-05-17 22:24:52 +0200

    fixed design bugs, info start

--- pages/info.js
+++ pages/info.js
@@ -5,105 +5,73 @@ export default function Info() {
     return (
         <>
             <div className='content'>
-                {/* <Image
-                    src='/static/public/images/Stroke-1.svg'
-                    loading='lazy'
-                    layout='fill'
-                    style={{ zIndex: 3000 }}
-                    //width="100%"
-                    alt=''
-                    className='divider info'
-                ></Image>
-                <div
-                    data-hover='false'
-                    data-delay='0'
-                    data-w-id='99263c60-1238-d19b-e78c-88a9cb6a1abb'
-                    style={{ height: '80px' }}
-                    //  {{marginRight: spacing + 'em'}}
-                    className='accordion-item w-dropdown'
-                ></div>
-                <div className='accordion-toggle w-dropdown-toggle'></div>
-                <div className='text-block-4'>INFO </div>
-                <div class='dropdown-list-2 w-dropdown-list'>
-                    <div class='section-2 wf-section'>
-                        <div
-                            id='w-node-_87433759-f5d8-27ae-c229-dbecd23288f3-8fcec08f'
-                            class='div-block-13'
-                        >
-                            <div className='myGrid'>
-                                <p class='paragraph-4'>
-                                    Im Kontext der Klimakrise und des damit
-                                    einhergehenden gesellschaftlichen Wandels
-                                    untersucht das Projekt die Beziehung
-                                    zwischen Berlin und der Spree, dem
-                                    städtischen Raum, den Bürgern und dem
-                                    Gewässer. Wie kann dem Fluss eine eigene
-                                    &quot;politische&quot; Stimme gegeben werden
-                                    und was kann dazu beitragen, mehr
-                                    Bewusstsein für die Spree und den Einfluss
-                                    des Menschen zu schaffen? Mit Hilfe der
-                                    Bojen macht das Gewässer auf die
-                                    Verschmutzung aufmerksam. Digitale Werkzeuge
-                                    und Medien, wie Messgeräte und App, erfassen
-                                    den aktuellen Zustand des Flusses und
-                                    vermitteln ihn den Bürgern auf zugängliche
-                                    Weise. Spannende historische und aktuelle
-                                    Artikel rund um das Thema werden ebenfalls
-                                    kommuniziert.Eine Zukunftsebene auf der
-                                    Website zeigt die ungenutzten Potenziale auf
-                                    und veranschaulicht, wie sich die Stadt mit
-                                    Hilfe des Flusses zum Besseren verändern
-                                    könnte, und regt Bürger und Politiker zum
-                                    Handeln an. In den heißen Sommermonaten
-                                    pumpt die Boje Sauerstoff in den Fluss und
-                                    versorgt die Spree und ihre Lebewesen damit.
-                                    Der planetenzentrierte Ansatz bietet eine
-                                    ganzheitliche und &quot;grüne&quot; Lösung
-                                    für die verschiedenen Probleme und sorgt für
-                                    ein gesünderes Gleichgewicht zwischen den
-                                    beteiligten Akteuren.
-                                </p>
-                                <Image
-                                    src='/static/images/SpreeBerlin_DASBILD_klein.jpg'
-                                    loading='lazy'
-                                    layout='fill'
-                                    id='w-node-_98dce9f7-74b7-abf6-f19e-8393d078b827-8fcec08f'
-                                    sizes='(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 98vw, 33vw'
-                                    alt='buoy - interactive'
-                                    class='image-8'
-                                ></Image>
-                                <Image
-                                    src='/static/images/Bouye03.jpg'
-                                    loading='lazy'
-                                    layout='fill'
-                                    id='w-node-_8eb5696d-b3f1-e515-9601-5a92f9838dbf-8fcec08f'
-                                    sizes='(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 98vw, 33vw'
-                                    alt='bouye - model'
-                                    className='image-8'
-                                ></Image>
-                                <Image
-                                    class='image-11-copy'
-                                    src='/static/images/HC-PCDesign.png'
-                                    layout='fill'
-                                    alt='planet-centric-design'
-                                    sizes='(max-width: 767px) 59vw, (max-width: 991px) 44vw, 19vw'
-                                    id='w-node-ef627c95-3e68-87f1-84f5-af309b2dacaa-8fcec08f'
-                                    loading='lazy'
-                                    className='image-8'
-                                ></Image>
-                                <Image
-                                    src='/static/images/SpreeVision01.png'
-                                    loading='lazy'
-                                    layout='fill'
-                                    id='w-node-_4ab63b06-4284-c243-d9b5-4c53888b80c5-8fcec08f'
-                                    sizes='(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 98vw, 33vw'
-                                    alt='Humboldthafen Entwurf'
-                                    className='image-8'
-                                ></Image>
-                            </div>
+                <div class="heading-4">INFO</div>
+                <div className='infoBox'>
+                    <div className='section-2'>
+                        <p className='flowing-text'>
+                            Im Kontext der Klimakrise und des damit
+                            einhergehenden gesellschaftlichen Wandels untersucht
+                            das Projekt die Beziehung zwischen Berlin und der
+                            Spree, dem städtischen Raum, den Bürgern und dem
+                            Gewässer. Wie kann dem Fluss eine eigene
+                            "politische" Stimme gegeben werden und was kann dazu
+                            beitragen, mehr Bewusstsein für die Spree und den
+                            Einfluss des Menschen zu schaffen? Mit Hilfe der
+                            Bojen macht das Gewässer auf die Verschmutzung
+                            aufmerksam. Digitale Werkzeuge und Medien, wie
+                            Messgeräte und App, erfassen den aktuellen Zustand
+                            des Flusses und vermitteln ihn den Bürgern auf
+                            zugängliche Weise. Spannende historische und
+                            aktuelle Artikel rund um das Thema werden ebenfalls
+                            kommuniziert. Eine Zukunftsebene auf der Website
+                            zeigt die ungenutzten Potenziale auf und
+                            veranschaulicht, wie sich die Stadt mit Hilfe des
+                            Flusses zum Besseren verändern könnte, und regt
+                            Bürger und Politiker zum Handeln an. In den heißen
+                            Sommermonaten pumpt die Boje Sauerstoff in den Fluss
+                            und versorgt die Spree und ihre Lebewesen damit. Der
+                            planetenzentrierte Ansatz bietet eine ganzheitliche
+                            und "grüne" Lösung für die verschiedenen Probleme
+                            und sorgt für ein gesünderes Gleichgewicht zwischen
+                            den beteiligten Akteuren.
+                        </p>
+                        <div className='myContainer'>
+                            <div className='myContainerItem'>
+                            <Image
+                                src={`/static/images/SpreeBerlin_DASBILD_klein.jpg`}
+                                width='375px'
+                                height='250px'
+                                 layout='fill'
+                            ></Image></div>
+                            <div className='myContainerItem'>
+                            <Image
+                                src={`/static/images/Bouye03.jpg`}
+                                width='375px'
+                                height='250px'
+                                layout='fill'
+                                //layout='intrinsic'
+                            ></Image></div>
+                        </div>
+                        <div className='myContainer'>
+                            <div className='myContainerItem'>
+                            <Image
+                                src={`/static/images/HC-PCDesign-p-500.png`}
+                                width='375px'
+                                height='250px'
+                                layout='fill'
+                                // layout='intrinsic'
+                            ></Image></div>
+                            <div className='myContainerItem'>
+                            <Image
+                                src={`/static/images/SpreeVision01.png`}
+                                width='375px'
+                                height='250px'
+                                layout='fill'
+                                //layout='intrinsic'
+                            ></Image></div>
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </>
     );
